5f532dc3f82f524d2eccb73630ac70f3726986dc
David Bueno <davidbuenox8@gmail.com>
2021-09-22 13:48:01 +0200

    register route added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,69 @@
 import mongoose, { Error } from 'mongoose';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import passport from 'passport';
 import passportLocal from 'passport-local';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import dotenv from 'dotenv';
+import User from './User';
+import { UserInterface } from './Interfaces/UserInterface';
 
 mongoose.connect(
-  'mongodb+srv://Davidx8:Patiment88@cluster0.gzlzd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  'mongodb+srv://Davidx8:Patiment88@cluster0.gzlzd.mongodb.net/gameShare?retryWrites=true&w=majority',
   // no longer needed :{ useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true },
   (err: Error) => {
     if (err) throw err;
     console.log('Connected to MongoDB');
   }
 );
+
+//Middlewares
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(
+  session({
+    secret: 'secretcode',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Passport
+
+//Routes
+app.post('/register', async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  if (
+    !username ||
+    !password ||
+    typeof username !== 'string' ||
+    typeof password !== 'string'
+  ) {
+    res.send('Improper Values');
+    return;
+  }
+
+  User.findOne({ username }, async (err: Error, doc: UserInterface) => {
+    if (err) throw err;
+    if (doc) res.send('User already exists');
+    if (!doc) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = new User({
+        username,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      res.send('Success');
+    }
+  });
+});
+
+app.listen(4000, () => {
+  console.log('Server Started');
+});
